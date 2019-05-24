80fef55246dff5cb03f5c69abc5c48a2f3d3eaea
Szymon Kamiński <kamin.szymon@gmail.com>
2019-05-24 16:51:36 +0200

    model created

--- src/main/java/com/zadanie/exchange/backend/controllers/Controller.java
+++ src/main/java/com/zadanie/exchange/backend/controllers/Controller.java
@@ -1,14 +1,13 @@
 package com.zadanie.exchange.backend.controllers;
 
+import com.zadanie.exchange.backend.model.Currencies;
 import com.zadanie.exchange.backend.service.ConnectionService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
 import java.util.Map;
-import java.util.Set;
 
 @RestController
 public class Controller {
@@ -16,16 +15,35 @@ public class Controller {
     @Autowired
     ConnectionService connectionService;
 
-    @GetMapping("/")
+    @GetMapping("/obj")
     @CrossOrigin(origins = "http://localhost:4200")
     public Map getCurrencyMap() throws IOException {
         return connectionService.getValuesMap();
     }
-
-    @GetMapping("/list")
+/*
+    @GetMapping("/exchangeCurrencies")
     @CrossOrigin(origins = "http://localhost:4200")
-    public Set getCurrencyKeyList() throws IOException {
-        return connectionService.getValuesList();
+    public String getCurrencyKeyList() {
+        return connectionService.getRealtimeCurrencyExchangeRate();
+    }*/
+
+    @GetMapping("/exchangeCurrencies")
+    @CrossOrigin(origins = "http://localhost:4200")
+    public String getJsonVar() throws IOException {
+        return connectionService.getJson();
     }
+
+    @PostMapping("/exchangeCurrencies")
+    @ResponseStatus(HttpStatus.ACCEPTED)
+    public Currencies find(@RequestBody Currencies currencies){
+        return currencies;
+    }
+
+  /*  @GetMapping("/post")
+    @CrossOrigin(origins = "http://localhost:4200")
+    public String get(){
+        return
+    }*/
+
 }
 
---
+++ src/main/java/com/zadanie/exchange/backend/model/Currencies.java
@@ -0,0 +1,23 @@
+package com.zadanie.exchange.backend.model;
+
+public class Currencies {
+
+    private String firstCur;
+    private String secondCur;
+
+    public String getFirstCur() {
+        return firstCur;
+    }
+
+    public void setFirstCur(String firstCur) {
+        this.firstCur = firstCur;
+    }
+
+    public String getSecondCur() {
+        return secondCur;
+    }
+
+    public void setSecondCur(String secondCur) {
+        this.secondCur = secondCur;
+    }
+}
